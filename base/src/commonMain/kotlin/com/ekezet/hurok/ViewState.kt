127ef6f78a03dac88465a67146213affcd1aa4c3
Kiripolszky Károly <karcsi@ekezet.com>
2024-08-25 10:15:20 +0200

    Add KDoc to ViewState

--- base/src/commonMain/kotlin/com/ekezet/hurok/ViewState.kt
+++ base/src/commonMain/kotlin/com/ekezet/hurok/ViewState.kt
@@ -1,5 +1,8 @@
 package com.ekezet.hurok
 
+/**
+ * ViewState is used to delegate the emitter to the rendered state for commiting actions.
+ */
 abstract class ViewState<TModel : Any, TDependency> {
     internal var emitter: ActionEmitter<TModel, TDependency>? = null
 
