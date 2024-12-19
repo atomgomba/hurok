f5ef93f14efa2499e2f5b1b8e27ba1e654187aa8
atomgomba <karcsi@ekezet.com>
2024-12-19 20:23:05 +0100

    refactor(Action): move helper functions out of interface

--- base/src/commonMain/kotlin/com/ekezet/hurok/Action.kt
+++ base/src/commonMain/kotlin/com/ekezet/hurok/Action.kt
@@ -1,5 +1,7 @@
 package com.ekezet.hurok
 
+import com.ekezet.hurok.Action.Next
+
 /**
  * Action is used to mutate the model and trigger effects.
  */
@@ -12,23 +14,23 @@ fun interface Action<TModel : Any, TDependency> {
         val effects: Set<Effect<TModel, TDependency>> = emptySet(),
     )
 
-    val skip: Next<TModel, TDependency>
-        get() = Next()
-
     fun TModel.proceed(): Next<TModel, TDependency>
-
-    fun outcome(
-        model: TModel? = null,
-        vararg effects: Effect<TModel, TDependency>,
-    ): Next<TModel, TDependency> =
-        Next(
-            model = model,
-            effects = effects.toSet(),
-        )
-
-    fun mutate(model: TModel? = null): Next<TModel, TDependency> =
-        Next(model = model)
-
-    fun trigger(vararg effects: Effect<TModel, TDependency>): Next<TModel, TDependency> =
-        Next(effects = effects.toSet())
 }
+
+val <TModel : Any, TDependency> Action<TModel, TDependency>.skip: Next<TModel, TDependency>
+    get() = Next()
+
+fun <TModel : Any, TDependency> Action<TModel, TDependency>.outcome(
+    model: TModel? = null,
+    vararg effects: Effect<TModel, TDependency>,
+): Next<TModel, TDependency> =
+    Next(
+        model = model,
+        effects = effects.toSet(),
+    )
+
+fun <TModel : Any, TDependency> Action<TModel, TDependency>.mutate(model: TModel? = null): Next<TModel, TDependency> =
+    Next(model = model)
+
+fun <TModel : Any, TDependency> Action<TModel, TDependency>.trigger(vararg effects: Effect<TModel, TDependency>): Next<TModel, TDependency> =
+    Next(effects = effects.toSet())
