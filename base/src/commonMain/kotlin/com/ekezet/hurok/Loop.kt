01c554b2ea6da6629b5f0b4cb65c5912a981b47f
atomgomba <karcsi@ekezet.com>
2025-01-20 17:28:36 +0100

    refactor(Loop)!: improve coroutine context management
    Pass `CoroutineContext` instead of `Dispatcher` for greater flexibility.
    
    Do not always launch a new coroutine for `onNextAction()` calls.

--- base/src/commonMain/kotlin/com/ekezet/hurok/Loop.kt
+++ base/src/commonMain/kotlin/com/ekezet/hurok/Loop.kt
@@ -1,10 +1,10 @@
 package com.ekezet.hurok
 
 import com.ekezet.hurok.test.CoverageIgnore
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,6 +12,7 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Base class that handles unidirectional data flow.
@@ -21,7 +22,7 @@ import kotlinx.coroutines.launch
  * @param args input arguments
  * @param firstAction action to emit when loop is constructed
  * @param dependency dependency container
- * @param effectDispatcher dispatcher for launching effects
+ * @param effectContext dispatcher for launching effects
  */
 abstract class Loop<out TState : ViewState<TModel, TDependency>, TModel : Any, in TArgs, TDependency, in TAction : Action<TModel, TDependency>>(
     model: TModel,
@@ -29,14 +30,14 @@ abstract class Loop<out TState : ViewState<TModel, TDependency>, TModel : Any, i
     args: TArgs? = null,
     private val firstAction: TAction? = null,
     private val dependency: TDependency? = null,
-    private val effectDispatcher: CoroutineDispatcher = DispatcherProvider.IO,
+    private val effectContext: CoroutineContext = DispatcherProvider.IO,
 ) : ActionEmitter<TModel, TDependency> {
 
-    private val _model = MutableStateFlow(model.run {
+    private val currentModel = MutableStateFlow(model.run {
         args?.let { applyArgs(it) } ?: this
     })
 
-    val state: Flow<TState> = _model.map(renderer::renderState).onEach {
+    val state: Flow<TState> = currentModel.map(renderer::renderState).onEach {
         it.emitter = this
     }
 
@@ -65,7 +66,7 @@ abstract class Loop<out TState : ViewState<TModel, TDependency>, TModel : Any, i
     }
 
     fun applyArgs(args: TArgs) {
-        _model.update { _model.value.applyArgs(args) }
+        currentModel.update { currentModel.value.applyArgs(args) }
     }
 
     fun startIn(parentScope: CoroutineScope): Loop<TState, TModel, TArgs, TDependency, TAction> {
@@ -85,15 +86,14 @@ abstract class Loop<out TState : ViewState<TModel, TDependency>, TModel : Any, i
         throw NotImplementedError("This method must be overridden to apply arguments")
     }
 
-    private fun onNextAction(action: TAction) = parentScope.launch {
-        val (updatedModel, triggeredEffects) = action.run { _model.value.proceed() }
-        updatedModel?.let { new -> _model.update { new } }
-        val effects = triggeredEffects.takeIf { it.isNotEmpty() } ?: return@launch
-        launch(effectDispatcher) {
-            val deferreds = effects.map { effect ->
-                async { effect.run { trigger(dependency) } }
-            }
-            deferreds.awaitAll()
+    private suspend fun onNextAction(action: TAction) = coroutineScope {
+        val (updatedModel, triggeredEffects) = action.run { currentModel.value.proceed() }
+        updatedModel?.let { new -> currentModel.update { new } }
+        val effects = triggeredEffects.takeIf { it.isNotEmpty() } ?: return@coroutineScope
+        launch(effectContext) {
+            effects
+                .map { effect -> async { effect.run { trigger(dependency) } } }
+                .awaitAll()
         }
     }
 }
