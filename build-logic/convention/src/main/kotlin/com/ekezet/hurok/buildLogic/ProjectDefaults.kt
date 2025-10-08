dcf816827cfc0b3eea010b8252714be9b0ba298e
atomgomba <karcsi@ekezet.com>
2025-10-08 11:57:05 +0200

    chore: JDK to 21 and minSdk to 26

--- build-logic/convention/src/main/kotlin/com/ekezet/hurok/buildLogic/ProjectDefaults.kt
+++ build-logic/convention/src/main/kotlin/com/ekezet/hurok/buildLogic/ProjectDefaults.kt
@@ -3,5 +3,5 @@ package com.ekezet.hurok.buildLogic
 import org.gradle.api.JavaVersion
 
 object ProjectDefaults {
-    val javaVersion: JavaVersion = JavaVersion.VERSION_17
+    val javaVersion: JavaVersion = JavaVersion.VERSION_21
 }
